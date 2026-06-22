05a49fc2e2ef5dada78af5a7ad0571ceb0ad95bb
Puneeth D <puneeth.cseaiml24le@cmrit.ac.in>
2026-06-22 10:01:43 +0530

    Add Ansible playbook and inventory setup

--- an.c
+++ an.c
@@ -6,3 +6,24 @@ source ~/.bashrc
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 ansible --version
+
+
+  student@dl003pc015-desktop:~$ gedit setup.yml
+setup.yml
+---
+- name: Message Playbook
+ hosts: local
+ gather_facts: no
+ tasks:
+ - name: Print Hello Message
+ debug:
+ msg: "Hello from Ansible"
+
+
+
+student@dl003pc015-desktop:~$ gedit hosts.ini
+hosts.ini
+[local]
+localhost ansible_connection=local
+student@dl003pc015-desktop:~$ ls
+-student@dl003pc015-desktop:~$ ansible-playbook -i hosts.ini setup.yml
